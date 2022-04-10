be42b18af070e6690cc85f903ca851e3e5aa7861
Oleksandr Yefremov <ext-oleksandr.yefremov@here.com>
2022-04-10 18:19:51 +0300

    fix ownership

--- asio_playground_main.cpp
+++ asio_playground_main.cpp
@@ -18,35 +18,45 @@ void async_sleep(asio::io_context &ctx, std::chrono::steady_clock::duration t,
       [timer, cb = std::forward<Callback>(cb)](std::error_code ec) { cb(); });
 }
 
+int num_in_flight = 0;
+
+auto start_work(asio::io_context& ctx) {
+
+
+    auto generate_itmes = [](unsigned n) {
+        std::vector<std::string> res;
+        while (n--) {
+            res.emplace_back(std::to_string(n));
+        }
+        return res;
+    };
+
+    auto items = generate_itmes(300);
+
+    auto foreach_ctx = bounded_async_foreach(
+        14, items, [&ctx](auto& item, auto done_cb) {
+            std::cout << "processing item [" << item << "] started "
+                << "  (num in flight: " << ++num_in_flight << ")\n";
+
+            // simulate long running operation and signal finish by calling done_cb
+            async_sleep(ctx, std::chrono::milliseconds((rand() % 5) * 100),
+                [item, done_cb = std::move(done_cb)]{
+                  std::cout << "processing item [" << item << "] done "
+                            << "     (num in flight: " << --num_in_flight
+                            << ")\n";
+                  done_cb();
+                });
+        });
+    return foreach_ctx;
+}
+
 int main() {
   std::cout << "asio playground\n";
 
   asio::io_context ctx;
 
-  int num_in_flight = 0;
 
-  auto generate_itmes = [](unsigned n) {
-    std::vector<std::string> res;
-    while (n--) {
-      res.emplace_back(std::to_string(n));
-    }
-    return res;
-  };
-
-  auto foreach_ctx = bounded_async_foreach(
-      14, generate_itmes(300), [&](auto &item, auto done_cb) {
-        std::cout << "processing item [" << item << "] started "
-                  << "  (num in flight: " << ++num_in_flight << ")\n";
-
-        // simulate long running operation and signal finish by calling done_cb
-        async_sleep(ctx, std::chrono::milliseconds((rand() % 5) * 100),
-                    [&num_in_flight, item, done_cb = std::move(done_cb)] {
-                      std::cout << "processing item [" << item << "] done "
-                                << "     (num in flight: " << --num_in_flight
-                                << ")\n";
-                      done_cb();
-                    });
-      });
+  auto foreach_ctx = start_work(ctx);
 
   // simulate cancel after 3s
   async_sleep(ctx, 2s, [&foreach_ctx]() {
