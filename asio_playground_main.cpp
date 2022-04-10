6ce8e8cb511310bea3039e72ae3ca6c586d8d4ce
Liubomyr Semkiv <liubomyr.semkiv@ringteam.com>
2022-04-10 16:16:37 +0300

    clean up comments

--- asio_playground_main.cpp
+++ asio_playground_main.cpp
@@ -16,9 +16,7 @@ void async_sleep(asio::io_context &ctx, std::chrono::steady_clock::duration t,
   auto timer = std::make_shared<asio::steady_timer>(ctx, t);
   timer->async_wait(
       [timer, cb = std::forward<Callback>(cb)](std::error_code ec) {
-        if (ec != asio::error::operation_aborted) {
-          cb();
-        }
+        cb();
       });
 }
 
@@ -37,15 +35,16 @@ int main() {
     return res;
   };
 
-  bounded_async_foreach(14, generate_itmes(100), [&](auto &item, auto done_cb) {
+  bounded_async_foreach(14, generate_itmes(300), [&](auto &item, auto done_cb) {
     std::cout << "processing item [" << item << "] started "
               << "  (num in flight: " << ++num_in_flight << ")\n";
 
-    // simulate long running operation
-    async_sleep(ctx, std::chrono::seconds(rand() % 4 + 1),
+    // simulate long running operation and signal finish by calling done_cb
+    async_sleep(ctx, std::chrono::milliseconds((rand() % 5) * 100),
                 [&num_in_flight, item, done_cb = std::move(done_cb)] {
                   std::cout << "processing item [" << item << "] done "
-                            << "     (num in flight: " << --num_in_flight << ")\n";
+                            << "     (num in flight: " << --num_in_flight
+                            << ")\n";
                   done_cb();
                 });
   });
--- bounded_async_foreach.hpp
+++ bounded_async_foreach.hpp
@@ -1,47 +1,40 @@
 #pragma once
 
-#include <array>
-#include <cstddef>
-#include <cstdio>
 #include <functional>
 #include <memory>
-#include <string>
-#include <tuple>
-#include <type_traits>
-#include <utility>
-#include <vector>
 
-// Apply callback to each element in container.
+// Apply callback to each element in container but limit number of in items
+// being processed.
+// Args:
 //      Container -- any contianer that has cbegin(), cend()
 //      Callback  -- callable object that accepts (const Value& v, Callback
 //      done) ).
 template <typename Container, typename Callback>
 void bounded_async_foreach(unsigned n, const Container &c, Callback &&cb) {
-
   using iter_t = decltype(std::cbegin(c));
   struct algorithm_state {
     iter_t it;
     unsigned n = 0;
-
     algorithm_state(iter_t it, unsigned n) : it(it), n(n) {}
   };
 
   auto shared_state = std::make_shared<algorithm_state>(std::cbegin(c), n);
-
   auto process_next = std::make_shared<std::function<void()>>(nullptr);
+
   *process_next = [process_next, shared_state, &cb, end_it = std::cend(c)]() {
     if (shared_state->it == end_it) {
       return;
     }
     if (shared_state->n > 0) {
-      shared_state->n--; // consume one vacant place
+      shared_state->n--;
       cb(*shared_state->it++, [process_next, shared_state]() {
-        shared_state->n++; // free one vacant place
+        // free one vacant place and start next one if there is any
+        shared_state->n++;
         (*process_next)();
         return;
       });
-      (*process_next)(); // start next one immidiately  if there is vacant place
-                         // in pool.
+      // process next if there is vacant place
+      (*process_next)();
     } else {
       // there are not vacant palces
     }
