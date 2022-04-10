3b37dc92a6725aa3e1ed9d105df9366be7bd81c3
Liubomyr Semkiv <sli.ukraine@gmail.com>
2022-04-10 16:40:53 +0300

    add simplest cancelling interface

--- asio_playground_main.cpp
+++ asio_playground_main.cpp
@@ -15,9 +15,7 @@ void async_sleep(asio::io_context &ctx, std::chrono::steady_clock::duration t,
                  Callback &&cb) {
   auto timer = std::make_shared<asio::steady_timer>(ctx, t);
   timer->async_wait(
-      [timer, cb = std::forward<Callback>(cb)](std::error_code ec) {
-        cb();
-      });
+      [timer, cb = std::forward<Callback>(cb)](std::error_code ec) { cb(); });
 }
 
 int main() {
@@ -35,18 +33,26 @@ int main() {
     return res;
   };
 
-  bounded_async_foreach(14, generate_itmes(300), [&](auto &item, auto done_cb) {
-    std::cout << "processing item [" << item << "] started "
-              << "  (num in flight: " << ++num_in_flight << ")\n";
+  auto foreach_ctx = bounded_async_foreach(
+      14, generate_itmes(300), [&](auto &item, auto done_cb) {
+        std::cout << "processing item [" << item << "] started "
+                  << "  (num in flight: " << ++num_in_flight << ")\n";
 
-    // simulate long running operation and signal finish by calling done_cb
-    async_sleep(ctx, std::chrono::milliseconds((rand() % 5) * 100),
-                [&num_in_flight, item, done_cb = std::move(done_cb)] {
-                  std::cout << "processing item [" << item << "] done "
-                            << "     (num in flight: " << --num_in_flight
-                            << ")\n";
-                  done_cb();
-                });
+        // simulate long running operation and signal finish by calling done_cb
+        async_sleep(ctx, std::chrono::milliseconds((rand() % 5) * 100),
+                    [&num_in_flight, item, done_cb = std::move(done_cb)] {
+                      std::cout << "processing item [" << item << "] done "
+                                << "     (num in flight: " << --num_in_flight
+                                << ")\n";
+                      done_cb();
+                    });
+      });
+
+  // simulate cancel after 3s
+  async_sleep(ctx, 2s, [&foreach_ctx]() {
+    std::cout << "time out, cancelling async foreach...\n";
+    foreach_ctx();
   });
+
   ctx.run();
 }
--- bounded_async_foreach.hpp
+++ bounded_async_foreach.hpp
@@ -10,7 +10,7 @@
 //      Callback  -- callable object that accepts (const Value& v, Callback
 //      done) ).
 template <typename Container, typename Callback>
-void bounded_async_foreach(unsigned n, const Container &c, Callback &&cb) {
+auto bounded_async_foreach(unsigned n, const Container &c, Callback &&cb) {
   using iter_t = decltype(std::cbegin(c));
   struct algorithm_state {
     iter_t it;
@@ -21,24 +21,38 @@ void bounded_async_foreach(unsigned n, const Container &c, Callback &&cb) {
   auto shared_state = std::make_shared<algorithm_state>(std::cbegin(c), n);
   auto process_next = std::make_shared<std::function<void()>>(nullptr);
 
-  *process_next = [process_next, shared_state, &cb, end_it = std::cend(c)]() {
-    if (shared_state->it == end_it) {
-      return;
-    }
-    if (shared_state->n > 0) {
-      shared_state->n--;
-      cb(*shared_state->it++, [process_next, shared_state]() {
-        // free one vacant place and start next one if there is any
-        shared_state->n++;
-        (*process_next)();
+  *process_next = [process_next,
+                   shared_state_weak =
+                       std::weak_ptr<algorithm_state>(shared_state),
+                   &cb, end_it = std::cend(c)]() {
+    if (auto shared_state = shared_state_weak.lock()) {
+      if (shared_state->it == end_it) {
         return;
-      });
-      // process next if there is vacant place
-      (*process_next)();
+      }
+      if (shared_state->n > 0) {
+        shared_state->n--;
+        cb(*shared_state->it++, [process_next, shared_state_weak]() {
+          if (auto shared_state = shared_state_weak.lock()) {
+            // free one vacant place and start next one if there is any
+            shared_state->n++;
+            (*process_next)();
+            return;
+          } else {
+            std::cout << "(dbg): cancalled\n";
+          }
+        });
+        // process next if there is vacant place
+        (*process_next)();
+      } else {
+        // there are not vacant palces
+      }
     } else {
-      // there are not vacant palces
+      std::cout << "(dbg): cancalled\n";
     }
   };
 
   (*process_next)();
+
+  auto ctx = [shared_state]() mutable { shared_state.reset(); };
+  return ctx;
 }
