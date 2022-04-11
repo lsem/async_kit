fe0090a4c740a15cfa993f7ba88af3641de5cbfa
Liubomyr Semkiv <liubomyr.semkiv@ringteam.com>
2022-04-11 10:56:34 +0300

    add finished callback support

--- asio_playground_main.cpp
+++ asio_playground_main.cpp
@@ -20,34 +20,38 @@ void async_sleep(asio::io_context &ctx, std::chrono::steady_clock::duration t,
 
 int num_in_flight = 0;
 
-auto start_work(asio::io_context& ctx) {
+auto start_work(asio::io_context &ctx) {
 
+  auto generate_itmes = [](unsigned n) {
+    std::vector<std::string> res;
+    while (n--) {
+      res.emplace_back(std::to_string(n));
+    }
+    return res;
+  };
 
-    auto generate_itmes = [](unsigned n) {
-        std::vector<std::string> res;
-        while (n--) {
-            res.emplace_back(std::to_string(n));
-        }
-        return res;
-    };
+  auto items = generate_itmes(300);
 
-    auto items = generate_itmes(300);
+  auto foreach_ctx = bounded_async_foreach(
+      14, items,
+      [&ctx](auto &item, auto done_cb) {
+        std::cout << "processing item [" << item << "] started "
+                  << "  (num in flight: " << ++num_in_flight << ")\n";
 
-    auto foreach_ctx = bounded_async_foreach(
-        14, items, [&ctx](auto& item, auto done_cb) {
-            std::cout << "processing item [" << item << "] started "
-                << "  (num in flight: " << ++num_in_flight << ")\n";
-
-            // simulate long running operation and signal finish by calling done_cb
-            async_sleep(ctx, std::chrono::milliseconds((rand() % 5) * 100),
-                [item, done_cb = std::move(done_cb)]{
-                  std::cout << "processing item [" << item << "] done "
-                            << "     (num in flight: " << --num_in_flight
-                            << ")\n";
-                  done_cb();
-                });
-        });
-    return foreach_ctx;
+        // simulate long running operation and signal finish by calling done_cb
+        async_sleep(ctx, std::chrono::milliseconds((rand() % 5) * 100),
+                    [item, done_cb = std::move(done_cb)] {
+                      std::cout << "processing item [" << item << "] done "
+                                << "     (num in flight: " << --num_in_flight
+                                << ")\n";
+                      done_cb();
+                    });
+      },
+      []() {
+        // finished
+        std::cout << "finished\n";
+      });
+  return foreach_ctx;
 }
 
 int main() {
@@ -55,14 +59,13 @@ int main() {
 
   asio::io_context ctx;
 
-
   auto foreach_ctx = start_work(ctx);
 
-  // simulate cancel after 3s
-  async_sleep(ctx, 2s, [&foreach_ctx]() {
-    std::cout << "time out, cancelling async foreach...\n";
-    foreach_ctx();
-  });
+  // // simulate cancel after 3s
+  // async_sleep(ctx, 2s, [&foreach_ctx]() {
+  //   std::cout << "time out, cancelling async foreach...\n";
+  //   foreach_ctx();
+  // });
 
   ctx.run();
 }
