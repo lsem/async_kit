9c37429dd3cc7ae4c814748670257e6c5b455df1
Liubomyr Semkiv <liubomyr.semkiv@ringteam.com>
2023-07-25 19:32:09 +0300

    Started working on asyncallback

--- include/async_kit/async_callback.hpp
+++ include/async_kit/async_callback.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
 #include <function2/function2.hpp>
+#include <system_error>
 
+template <class T>
+struct func_type {
+    using async_callback = fu2::unique_function<void(std::error_code, T)>;
+};
 
-//template <class T>
-//using async_callback  
+template <>
+struct func_type<void> {
+    using async_callback = fu2::unique_function<void(std::error_code)>;
+};
+
+template <class T>
+using async_callback = typename func_type<T>::async_callback;
