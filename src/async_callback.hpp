5d4a939f7038f11a663ff8050a34a21bdbdcf95c
Liubomyr Semkiv <ext-liubomyr.semkiv@here.com>
2023-08-03 23:31:31 +0300

    WIP: use real async_callback

--- src/async_callback.hpp
+++ src/async_callback.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <function2/function2.hpp>
+#include <iostream>
+#include <memory>
 #include <system_error>
 
 namespace lsem::async_kit {
@@ -32,7 +34,83 @@ struct func_type<void> {
     using async_callback = fu2::unique_function<void(std::error_code)>;
 };
 
-template <class T>
-using async_callback = typename func_type<T>::async_callback;
+struct default_log_fns_t {
+    static void print_error_line(const char* msg) { std::cerr << "ERROR: " << msg << "\n"; }
+};
+
+template <class T, class LogFns = default_log_fns_t>
+class async_callback_impl_t {
+   public:
+    async_callback_impl_t() : m_cb(nullptr) {}
+
+    template <class Callable>
+    async_callback_impl_t(Callable cb) : m_cb(std::move(cb)) {}
+
+    ~async_callback_impl_t() { handle_not_called(); }
+
+    async_callback_impl_t(const async_callback_impl_t&) = delete;
+    async_callback_impl_t& operator=(const async_callback_impl_t&) = delete;
+
+    async_callback_impl_t(async_callback_impl_t&& rhs) {
+        m_cb = std::move(rhs.m_cb);
+        m_called = rhs.m_called;
+        rhs.m_cb = nullptr;  // TODO: try swap/exhange instead once we have test that covers this.
+    }
+
+    async_callback_impl_t& operator=(async_callback_impl_t&& rhs) {
+        handle_not_called();
+        m_cb = std::move(rhs.m_cb);
+        m_called = rhs.m_called;
+        rhs.m_cb = nullptr;
+        return *this;
+    }
+
+    template <typename EnableWhenVoid = std::enable_if<std::is_same_v<T, void>>,
+              typename = typename EnableWhenVoid::type>
+    void operator()(const std::error_code& ec) {
+        if (!m_called) {
+            m_called = true;
+            m_cb(ec);
+        } else {
+            LogFns::print_error_line("async_kit: attempt to call the callback twice");
+            return;
+        }
+    }
+
+    template <typename Param = T,
+              typename EnableWhenNonVoid = std::enable_if<!std::is_same_v<T, void>>,
+              typename = typename EnableWhenNonVoid::type>
+    void operator()(const std::error_code& ec, Param&& p) {
+        if (!m_called) {
+            m_called = true;
+            m_cb(ec, std::forward<Param>(p));
+        } else {
+            LogFns::print_error_line("async_kit: attempt to call the callback twice");
+            return;
+        }
+    }
+
+    operator bool() const { return m_cb.operator bool(); }
+
+   private:
+    void handle_not_called() {
+        if (m_cb) {
+            if (!m_called) {
+                LogFns::print_error_line("async_kit: callback not called");
+
+                // TODO: this is working only or void callback.
+                if constexpr (std::is_invocable_v<decltype(m_cb), std::error_code>) {
+                    std::exchange(m_cb, nullptr)(make_error_code(errors::async_callback_err::not_called));
+                } else {
+                    std::exchange(m_cb, nullptr)(make_error_code(errors::async_callback_err::not_called), {});
+                }
+            }
+        }
+    }
+
+   private:
+    typename func_type<T>::async_callback m_cb;
+    bool m_called = false;
+};
 
 }  // namespace lsem::async_kit
--- src/async_callback_test.cpp
+++ src/async_callback_test.cpp
@@ -9,24 +9,51 @@
 #include <gtest/gtest.h>
 
 namespace errors = lsem::async_kit::errors;
-using lsem::async_kit::async_callback;
+
+template <class T>
+using tested_async_callback = lsem::async_kit::async_callback_impl_t<T>;
 
 using testing::ElementsAre;
 
-TEST(async_callback_test, basic_test__not_called) {
+TEST(async_callback_test, basic_test__not_called__void) {
     std::vector<std::error_code> invocations;
     {
-        async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
+        tested_async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
         EXPECT_TRUE(cb);
     }
     // callback gets auto called during destruction if not called manually.
     EXPECT_THAT(invocations, ElementsAre(make_error_code(errors::async_callback_err::not_called)));
 }
 
-TEST(async_callback_test, basic_test__called_twice) {
+TEST(async_callback_test, basic_test__not_called__param) {
+    std::vector<std::pair<std::error_code, int>> invocations;
+    {
+        tested_async_callback<int> cb = [&](std::error_code ec, int arg) { invocations.emplace_back(ec, arg); };
+        EXPECT_TRUE(cb);
+    }
+    // callback gets auto called during destruction if not called manually.
+    EXPECT_THAT(invocations, ElementsAre(std::pair<std::error_code, int>{
+                                 make_error_code(errors::async_callback_err::not_called), int{}}));
+}
+
+TEST(async_callback_test, calling_with_default_initialier__void) {
+    std::vector<std::error_code> invocations;
+    tested_async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
+    cb({});
+    EXPECT_THAT(invocations, ElementsAre(std::error_code{}));
+}
+
+TEST(async_callback_test, calling_with_default_initialier__param) {
+    std::vector<std::pair<std::error_code, int>> invocations;
+    tested_async_callback<int> cb = [&](std::error_code ec, int arg) { invocations.emplace_back(ec, arg); };
+    cb({}, 11);
+    EXPECT_THAT(invocations, ElementsAre(std::pair<std::error_code, int>{std::error_code{}, 11}));
+}
+
+TEST(async_callback_test, basic_test__called_twice__void) {
     std::vector<std::error_code> invocations;
     {
-        async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
+        tested_async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
         cb(std::error_code());
         cb(std::error_code());
         EXPECT_TRUE(cb);
@@ -35,15 +62,32 @@ TEST(async_callback_test, basic_test__called_twice) {
     EXPECT_THAT(invocations, ElementsAre(std::error_code()));
 }
 
-TEST(async_callback_test, empty_callback_does_nothing) {
-    async_callback<void> cb;
+TEST(async_callback_test, basic_test__called_twice__param) {
+    std::vector<std::pair<std::error_code, int>> invocations;
+    {
+        tested_async_callback<int> cb = [&](std::error_code ec, int arg) { invocations.emplace_back(ec, arg); };
+        cb(std::error_code(), 10);
+        cb(std::error_code(), 20);
+        EXPECT_TRUE(cb);
+    }
+    // callback gets auto called during destruction if not called manually.
+    EXPECT_THAT(invocations, ElementsAre(std::pair<std::error_code, int>{std::error_code(), 10}));
+}
+
+TEST(async_callback_test, empty_callback_does_nothing__void) {
+    tested_async_callback<void> cb;
     EXPECT_FALSE(cb);
 }
 
-TEST(async_callback_test, move_constructor_test) {
+TEST(async_callback_test, empty_callback_does_nothing__param) {
+    tested_async_callback<int> cb;
+    EXPECT_FALSE(cb);
+}
+
+TEST(async_callback_test, move_constructor_test__void) {
     std::vector<std::error_code> invocations;
     {
-        async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
+        tested_async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
         auto cb_copy = std::move(cb);
         EXPECT_FALSE(cb);
     }
@@ -51,15 +95,27 @@ TEST(async_callback_test, move_constructor_test) {
     EXPECT_THAT(invocations, ElementsAre(make_error_code(errors::async_callback_err::not_called)));
 }
 
-TEST(async_callback_test, assignment_constructor) {
+TEST(async_callback_test, move_constructor_test__param) {
+    std::vector<std::pair<std::error_code, int>> invocations;
+    {
+        tested_async_callback<int> cb = [&](std::error_code ec, int arg) { invocations.emplace_back(ec, arg); };
+        auto cb_copy = std::move(cb);
+        EXPECT_FALSE(cb);
+    }
+    // callback gets auto called during destruction if not called manually.
+    EXPECT_THAT(invocations, ElementsAre(std::pair<std::error_code, int>{
+                                 make_error_code(errors::async_callback_err::not_called), int{}}));
+}
+
+TEST(async_callback_test, assignment_constructor__void) {
     std::vector<std::error_code> a_invocations;
     std::vector<std::error_code> b_invocations;
     {
-        async_callback<void> a_cb = [&](std::error_code ec) { a_invocations.emplace_back(ec); };
-        async_callback<void> b_cb = [&](std::error_code ec) { b_invocations.emplace_back(ec); };
+        tested_async_callback<void> a_cb = [&](std::error_code ec) { a_invocations.emplace_back(ec); };
+        tested_async_callback<void> b_cb = [&](std::error_code ec) { b_invocations.emplace_back(ec); };
 
         b_cb = std::move(a_cb);
-        EXPECT_FALSE(b_cb);
+        EXPECT_FALSE(a_cb);
     }
 
     // callback gets auto called during destruction if not called manually.
@@ -67,12 +123,41 @@ TEST(async_callback_test, assignment_constructor) {
     EXPECT_THAT(b_invocations, ElementsAre(make_error_code(errors::async_callback_err::not_called)));
 }
 
-TEST(async_callback_test, null_assignment) {
+TEST(async_callback_test, assignment_constructor__param) {
+    std::vector<std::pair<std::error_code, int>> a_invocations;
+    std::vector<std::pair<std::error_code, int>> b_invocations;
+    {
+        tested_async_callback<int> a_cb = [&](std::error_code ec, int arg) { a_invocations.emplace_back(ec, arg); };
+        tested_async_callback<int> b_cb = [&](std::error_code ec, int arg) { b_invocations.emplace_back(ec, arg); };
+
+        b_cb = std::move(a_cb);
+        EXPECT_FALSE(a_cb);
+    }
+
+    // callback gets auto called during destruction if not called manually.
+    EXPECT_THAT(a_invocations, ElementsAre(std::pair<std::error_code, int>{
+                                   make_error_code(errors::async_callback_err::not_called), int{}}));
+    EXPECT_THAT(b_invocations, ElementsAre(std::pair<std::error_code, int>{
+                                   make_error_code(errors::async_callback_err::not_called), int{}}));
+}
+
+TEST(async_callback_test, null_assignment__void) {
     std::vector<std::error_code> invocations;
     {
-        async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
+        tested_async_callback<void> cb = [&](std::error_code ec) { invocations.emplace_back(ec); };
         auto cb_copy = nullptr;
     }
     // callback gets auto called during destruction if not called manually.
     EXPECT_THAT(invocations, ElementsAre(make_error_code(errors::async_callback_err::not_called)));
+}
+
+TEST(async_callback_test, null_assignment__param) {
+    std::vector<std::pair<std::error_code, int>> invocations;
+    {
+        tested_async_callback<int> cb = [&](std::error_code ec, int arg) { invocations.emplace_back(ec, arg); };
+        auto cb_copy = nullptr;
+    }
+    // callback gets auto called during destruction if not called manually.
+    EXPECT_THAT(invocations, ElementsAre(std::pair<std::error_code, int>{
+                                 make_error_code(errors::async_callback_err::not_called), int{}}));
 }
